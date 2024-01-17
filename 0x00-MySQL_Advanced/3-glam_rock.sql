566fe3af0ce437b9e93d5874dbadd355825e2667
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-18 04:31:21 +0800

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,13 +2,7 @@
 -- Task: Old school band
 
 -- List bands with Glam rock as their main style, ranked by longevity
-SELECT band_name,
-       IF(splitted[2] = 0, 0, 2022 - CAST(splitted[2] AS SIGNED) + 1) as lifespan
-FROM (
-    SELECT band_name,
-           SPLIT_STR(SPLIT_STR(formed, '-', 1), ' ', 3) as splitted
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', style) > 0
-) as subquery
-ORDER BY lifespan DESC, band_name;
-
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
