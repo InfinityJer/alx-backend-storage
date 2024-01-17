e659c9c495b6881da69ee37ca1807151eafb76d0
InfinityJer <jclaris6@gmail.com>
2024-01-17 20:40:53 +0000

    8. Optimize simple search

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,5 @@
+-- 8-index_my_names.sql
+-- Task: Optimize simple search
+
+-- Create an index idx_name_first on the first letter of the name column
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
