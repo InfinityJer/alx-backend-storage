b49a6bacc0683d7b4d5ca4fe0f94a6166ecea3a2
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-18 04:45:44 +0800

    Update 9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -2,4 +2,4 @@
 -- Task: Optimize search and score
 
 -- Create an index idx_name_first_score on the first letter of the name and score columns
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names(name(1), score);
