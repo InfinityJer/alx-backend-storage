092d2a1dc6cfbb796d5a5650398594cc106b7cf4
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-01-25 19:19:13 +0800

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,18 +16,22 @@ def data_cacher(method: Callable) -> Callable:
     '''Caches the output of fetched data.
     '''
     @wraps(method)
-    def invoker(url: str) -> str:
+    def invoker(url) -> str:
         '''The wrapper function for caching the output.
         '''
+        # Increment the access count for the URL
         redis_store.incr(f'count:{url}')
+
+        # Retrieve cached result from Redis
         result = redis_store.get(f'result:{url}')
+
+        # return cached reults; otherwise, fetch and cache result
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+        redis_store.set(f'count:{url}', 0)  # Reset access count
+        redis_store.setex(f'result:{url}', 10, result)  
         return result
-
     return invoker
 
 
@@ -36,16 +40,5 @@ def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
     and tracking the request.
     '''
+    # Fetch the HTML content of the URL using requests
     return requests.get(url).text
-
-
-if __name__ == "__main__":
-    # Example usage
-    url_to_fetch = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://www.example.com"
-    html_content = get_page(url_to_fetch)
-    print(html_content)
-
-    # Check the access count for the URL
-    access_count_key = f"count:{url_to_fetch}"
-    access_count = redis_store.get(access_count_key)
-    print(f"The URL has been accessed {int(access_count)} times.")
