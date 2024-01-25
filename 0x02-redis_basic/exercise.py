7dd412fb7b628c093e617df6227935b17937f866
InfinityJer <jclaris6@gmail.com>
2024-01-25 11:07:31 +0000

    updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,6 @@ def count_calls(method: Callable) -> Callable:
     def wrapper(self, *args, **kwargs) -> Any:
         '''returns the given method after incrementing its call counter.
         '''
-
         # Increment call counter in Redis
         if isinstance(self._redis, redis.Redis):
             self._redis.incr(method.__qualname__)
@@ -33,20 +32,20 @@ def call_history(method: Callable) -> Callable:
         # Set up keys for storing input and output history
         in_key = '{}:inputs'.format(method.__qualname__)
         out_key = '{}:outputs'.format(method.__qualname__)
-        
+
         # Store input in Redis
         if isinstance(self._redis, redis.Redis):
             self._redis.rpush(in_key, str(args))
-            
+
         # Execute the method and get its output
         output = method(self, *args, **kwargs)
-        
+
         # Store output in Redis
         if isinstance(self._redis, redis.Redis):
             self._redis.rpush(out_key, output)
-            
+
         return output
-        
+
     return invoker
 
 
@@ -57,11 +56,11 @@ def replay(fn: Callable) -> None:
     if fn is None or not hasattr(fn, '__self__'):
         return
     redis_store = getattr(fn.__self__, '_redis', None)
-    
+
     # Check if the Redis instance is valid
     if not isinstance(redis_store, redis.Redis):
         return
-        
+
     # Retrieve function name, input and output keys, and call count
     fxn_name = fn.__qualname__
     in_key = '{}:inputs'.format(fxn_name)
@@ -69,10 +68,10 @@ def replay(fn: Callable) -> None:
     fxn_call_count = 0
     if redis_store.exists(fxn_name) != 0:
         fxn_call_count = int(redis_store.get(fxn_name))
-        
+
     # Display function call count
     print('{} was called {} times:'.format(fxn_name, fxn_call_count))
-    
+
     # Retrieve and display input and output history
     fxn_inputs = redis_store.lrange(in_key, 0, -1)
     fxn_outputs = redis_store.lrange(out_key, 0, -1)
@@ -95,7 +94,7 @@ class Cache:
 
     @call_history
     @count_calls
-    def store(self, data:  Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Stores a value in a Redis data storage and returns the key.
         '''
         # Generate a unique key for the data and store it in Redis
@@ -107,7 +106,7 @@ class Cache:
             self,
             key: str,
             fn: Callable = None,
-            ) -> Union[str, bytes, int, float]:
+    ) -> Union[str, bytes, int, float]:
         '''Retrieves a value from a Redis data storage.
         '''
         # Retrieve data from Redis and apply conversion function if provided
